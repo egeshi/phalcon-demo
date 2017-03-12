c0e3395c93b10e9145966ff9762a38d454d9e781
Antony Repin <lehcode@gmail.com>
2017-03-12 02:07:22 +0200

    Code format and date formta fix due to mismatch among dev and remote systems

--- app/src/Demo/Models/Traits/TimestampableEntity.php
+++ app/src/Demo/Models/Traits/TimestampableEntity.php
@@ -15,7 +15,8 @@ trait TimestampableEntity
      * @return $this
      * @throws ApplicationException
      */
-    public function setCreated(){
+    public function setCreated()
+    {
         $date = self::validDate();
         $this->created = $date;
         return $this;
@@ -25,23 +26,26 @@ trait TimestampableEntity
      * @return $this
      * @throws ApplicationException
      */
-    public function setUpdated(){
+    public function setUpdated()
+    {
         $date = self::validDate();
-        $this->updated =$date;
+        $this->updated = $date;
         return $this;
     }
 
     /**
      * @return \DateTime
      */
-    public function getUpdated(){
+    public function getUpdated()
+    {
         return $this->updated;
     }
 
     /**
      * @return \DateTime
      */
-    public function getCreated(){
+    public function getCreated()
+    {
         return $this->created;
     }
 
@@ -50,18 +54,20 @@ trait TimestampableEntity
      * @return \DateTime
      * @throws ApplicationException
      */
-    private function validDate(\DateTime $date=null)
+    private function validDate(\DateTime $date = null)
     {
+
+        $format = "Y-m-d H:i:s";
 
         if (!is_null($date)) {
             if (is_a($date, "DateTime")) {
-                return $date->format(\DateTime::W3C);
+                return $date->format($format);
             }
             throw new ApplicationException("Wrong date datatype", 500);
         }
 
         $now = new \DateTime();
-        return $now->format(\DateTime::W3C);
+        return $now->format($format);
 
     }
 }
